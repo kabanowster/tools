bb16f79aba882d57a4c1f67e8e4225db7aa9811b
Wiktor Kabanow <wiktor.kabanow@develey.pl>
2024-03-27 18:45:09 +0100

    Convention plugins revamp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,7 @@
-import java.net.URI
-
 plugins {
-    `java-library`
+    id("setup")
     id("myJavaFx")
-    id("coreTools")
-    `maven-publish`
-    id("net.linguica.maven-settings") version "0.5"
+    id("publish")
 }
 
 group = "io.krystal"
@@ -13,13 +9,11 @@ version = "1.0.0"
 
 java {
     withJavadocJar()
-    withSourcesJar()
 }
 
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
-            artifactId = "tools"
             from(components["java"])
             versionMapping {
                 usage("java-api") {
@@ -48,45 +42,4 @@ publishing {
             }
         }
     }
-    repositories {
-        maven {
-            name = "BDE-Development"
-            url = URI("https://dgd365o.pkgs.visualstudio.com/aa703476-5cc4-43ae-82af-8acbab9dab87/_packaging/BDE-Development/maven/v1")
-            authentication {
-                create<BasicAuthentication>("basic")
-            }
-        }
-    }
 }
-
-tasks.javadoc {
-    if (JavaVersion.current().isJava9Compatible) {
-        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
-    }
-}
-//tasks.jar {
-////    destinationDirectory.set(file("C:\\Users\\wa_kabanow\\Downloads"))
-//W
-//    from("src/main/java")
-//    exclude("*.java")
-//}
-
-//publishing {
-//    // TODO generalize
-//    repositories {
-//        maven {
-//            name = "GitHubPackages"
-//            url = URI("https://maven.pkg.github.com/kabanowster/SOURCE")
-//            credentials {
-//                username = "kabanowster"
-//                password = "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"
-//            }
-//        }
-//    }
-//
-//    publications {
-//        register<MavenPublication>("gpr") {
-//            from(components["java"])
-//        }
-//    }
-//}
